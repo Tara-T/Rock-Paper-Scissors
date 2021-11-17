21aa137814b6119fe0c4928ce8fc5d621230aff6
tara <tara.trbos@gmail.com>
2021-11-17 19:11:25 +0100

    Added stuff

--- main.js
+++ main.js
@@ -1,45 +1,92 @@
 let bot = 0;
 let player = 0;
-
-
 const options = document.querySelectorAll(".options")
+const botOptions = document.querySelectorAll('.bot-option')
+let score = document.getElementById("score");
+let startBtn = document.getElementById("start");
+let timer = document.getElementById("timer");
+let result = document.getElementById("result");
 
-options.forEach((option) => {
-    option.addEventListener("click", function () {
-        const pInput = this.getAttribute("id");
+function pick() {
+    options.forEach((option) => {
+        option.addEventListener("click", function game() {
+            const pInput = this.getAttribute("id");
 
-        this.style.border = "black 1px solid"; //TO DO: change this so other elements dissapear
+            this.style.border = "black 1px solid"; //TODO: change this so other elements dissapear
 
-        const cOptions = ["Rock", "Paper", "Scissors"];
-        const cInput = cOptions[Math.floor(Math.random() * 3)];
+            const cOptions = ["Rock", "Paper", "Scissors"];
+            const cInput = cOptions[Math.floor(Math.random() * 3)];
 
-        compareImp(pInput, cInput);
-        console.log(pInput , cInput);
-        console.log(bot, player);
+            border(cInput)
+            
+            
+            compareImp(pInput, cInput)
+            score.innerHTML = `${player} vs ${bot}`; 
+
+            console.log(pInput, cInput);
+            console.log(bot, player);
+
+        });
+    })
+};
+
+function border(cInput) { //making a full finction just to put a border >:(
+        if (cInput == "Rock") {
+            document.getElementById("cRock").style.border = "solid 1px black"
+        }
+        else if (cInput == "Paper") {
+            document.getElementById("cPaper").style.border = "solid 1px black"
+        }
+        else if (cInput == "Scissors"){
+            document.getElementById("cScissors").style.border = "solid 1px black"
+        }
+    };
         
-    });
-});
 
 
-//setTimeout(() => console.log("I will run every second"), 1000) //Make a timer
 
 function compareImp(pInput, cInput) {
-    const currentMatch = `${pInput} vs ${cInput}`
 
 
     if (pInput === cInput) {
-        document.getElementById('result').innerHTML = "its a tie";
-        return;
+        result.innerHTML = "It's a tie";
     }
 
-    if (pInput == "Rock" && cInput == "Scissors" || pInput == "Scissors" && cInput == "Paper" || pInput == "Paper" && cInput == "Rock") {
-        document.getElementById('result').innerHTML = "You win";
-        player+=1;
+    if (pInput === "Rock" && cInput === "Scissors" || pInput === "Scissors" && cInput === "Paper" || pInput === "Paper" && cInput === "Rock") {
+        result.innerHTML = "You win";
+        player++;
     }
-    else if (pInput == "Scissors" && cInput == "Rock" || pInput == "Paper" && cInput == "Scissors" || pInput == "Rock" && cInput == "Paper") {
-        document.getElementById('result').innerHTML = "You lose";
-        bot+=1;
+    else if (pInput === "Scissors" && cInput === "Rock" || pInput === "Paper" && cInput === "Scissors" || pInput === "Rock" && cInput === "Paper") {
+        result.innerHTML = "You lose";
+        bot++;
     }
 
 
 }
+let timeLeft = 3;
+
+function getTime() {
+    let timer = setInterval(function () {
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+        }
+        document.getElementById('timer').innerHTML = timeLeft;
+        timeLeft -= 1;
+    }, 1000);
+}
+
+
+startBtn.addEventListener('click', game);
+
+function game() { 
+   // while (bot != 3 && player != 3) maybe a for loop is better 
+    startBtn.style.display = 'none';
+    timer.style.display = 'block';
+    getTime(); //TODO fix delay
+    pick()
+   
+   
+}
+
+    
+
