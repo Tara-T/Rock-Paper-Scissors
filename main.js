8df0da59a9baa4761487b06cdd1af13a4f42880d
tara <tara.trbos@gmail.com>
2021-11-18 11:40:47 +0100

    timer is no longer decoration

--- main.js
+++ main.js
@@ -1,52 +1,60 @@
 let bot = 0;
 let player = 0;
+let timeLeft = 3;
+let timer;
+let pInput;
+let cInput;
 const options = document.querySelectorAll(".options")
 const botOptions = document.querySelectorAll('.bot-option')
 let score = document.getElementById("score");
 let startBtn = document.getElementById("start");
-let timer = document.getElementById("timer");
+let time = document.getElementById("timer");
 let result = document.getElementById("result");
 
-function pick() {
+function pick() { //this needs a better name
     options.forEach((option) => {
-        option.addEventListener("click", function game() {
-            const pInput = this.getAttribute("id");
+        option.addEventListener("click", function () {
+            pInput = this.getAttribute("id");
 
             this.style.border = "black 1px solid"; //TODO: change this so other elements dissapear
 
             const cOptions = ["Rock", "Paper", "Scissors"];
-            const cInput = cOptions[Math.floor(Math.random() * 3)];
+            cInput = cOptions[Math.floor(Math.random() * 3)];
 
             border(cInput)
-            
-            
-            compareImp(pInput, cInput)
-            score.innerHTML = `${player} vs ${bot}`; 
+
+            clearInterval(timer)
+
+            checkTime();
+            console.log(timeLeft)
+
+
+            score.innerHTML = `${player} vs ${bot}`;
 
             console.log(pInput, cInput);
-            console.log(bot, player);
+            console.log(player, bot);
 
         });
     })
 };
 
 function border(cInput) { //making a full finction just to put a border >:(
-        if (cInput == "Rock") {
-            document.getElementById("cRock").style.border = "solid 1px black"
-        }
-        else if (cInput == "Paper") {
-            document.getElementById("cPaper").style.border = "solid 1px black"
-        }
-        else if (cInput == "Scissors"){
-            document.getElementById("cScissors").style.border = "solid 1px black"
-        }
-    };
-        
 
+    switch (cInput) {
+        case "Rock":
+            document.getElementById("cRock").style.border = "solid 1px black"
+            break;
+        case "Paper":
+            document.getElementById("cPaper").style.border = "solid 1px black"
+            break;
+        case "Scissors":
+            document.getElementById("cScissors").style.border = "solid 1px black"
+            break;
+    }
+};
 
 
 function compareImp(pInput, cInput) {
-
 
     if (pInput === cInput) {
         result.innerHTML = "It's a tie";
@@ -60,33 +68,45 @@ function compareImp(pInput, cInput) {
         result.innerHTML = "You lose";
         bot++;
     }
-
-
 }
-let timeLeft = 3;
 
 function getTime() {
-    let timer = setInterval(function () {
+    timer = setInterval(function () {
         if (timeLeft <= 0) {
             clearInterval(timer);
         }
-        document.getElementById('timer').innerHTML = timeLeft;
+        time.innerHTML = timeLeft;
         timeLeft -= 1;
     }, 1000);
 }
 
-
-startBtn.addEventListener('click', game);
-
-function game() { 
-   // while (bot != 3 && player != 3) maybe a for loop is better 
-    startBtn.style.display = 'none';
-    timer.style.display = 'block';
-    getTime(); //TODO fix delay
-    pick()
-   
-   
+function checkTime() {
+    switch (timeLeft) {
+        case 0:
+            bot++;
+            result.innerHTML = "Too late"
+            break;
+        case 1:
+            console.log("case 1")
+            compareImp(pInput, cInput)
+            
+            break;
+        case 2:
+        case 3:
+            bot++;
+            result.innerHTML = "Too early"
+            break;
+    }
 }
 
-    
+function game() {
+    // while (bot != 3 && player != 3) maybe a for loop is better 
+    startBtn.style.display = 'none';
+    time.style.display = 'block';
+    getTime(); //TODO fix delay 
+    pick();
 
+
+}
+
+startBtn.addEventListener('click', game);
